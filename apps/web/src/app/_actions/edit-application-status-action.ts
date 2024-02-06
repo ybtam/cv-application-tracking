506353125bc4652c7f5f4ea5ecdcd85b53feae2a
Yi Tam <tam.yi3d@gmail.com>
2024-02-06 21:56:52 +0100

    added todo

--- apps/web/src/app/_actions/edit-application-status-action.ts
+++ apps/web/src/app/_actions/edit-application-status-action.ts
@@ -6,5 +6,7 @@ import {revalidatePath} from "next/cache";
 export default async function EditApplicationStatusAction(id: string, status: JobApplicationStatus) {
   console.log(id, status);
 
+  //todo
+
   revalidatePath("/")
 }
